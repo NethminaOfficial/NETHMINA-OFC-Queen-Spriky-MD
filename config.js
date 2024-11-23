c049bcbfe449f53585d7f5f1129e438f16e8ea0f
Nethmina Official <nethminaofc@gmail.com>
2024-11-23 07:59:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'biAyVKYK#TE-VLthmKmMZtEhcq432gJ6AVqIXYTISNpVrnscMkUY', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94706369728',    // Enter Your Owner Number
 };
